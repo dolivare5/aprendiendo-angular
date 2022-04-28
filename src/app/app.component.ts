2a0f43a3356bed9c65d00bbda3851c6d8369c1b8
dolivare5 <deiverolivares2@gmail.com>
2022-04-28 09:14:19 -0500

    Indentación corregida.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    // templateUrl: './app.component.html'
+    template: ` <h1></h1> `,
 })
 export class AppComponent {
-  title = 'bases';
+    title = './bases';
 }
